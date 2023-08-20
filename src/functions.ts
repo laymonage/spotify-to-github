1d00cfb55d5738f376c0734a2cb4c34f16daab37
Sage Abdullah <me@laymonage.com>
2023-08-20 20:58:11 +0100

    Add function to get Set difference

--- src/functions.ts
+++ src/functions.ts
@@ -21,3 +21,11 @@ export function* chunk<T>(arr: T[], n: number): Generator<T[], void> {
     yield arr.slice(i, i + n);
   }
 }
+
+export function difference<T>(setA: Set<T>, setB: Set<T>) {
+  const _difference = new Set(setA);
+  for (const elem of setB) {
+    _difference.delete(elem);
+  }
+  return _difference;
+}
